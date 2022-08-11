1c91fb0584e9407b53af1aeaaf700e4a77fdfe63
rory <rorymalone@live.com>
2022-08-11 02:41:32 +0100

    welcome page and more front end design added

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,19 @@
             <a href="/">
                 <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
             </a>
+            <div class="sm:text-center lg:text-left">
+                <h1
+                    class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl"
+                >
+                    <span class="block xl:inline">Bio-Infor</span>
+                    <span class="block text-indigo-600 xl:inline"
+                        >App
+                    </span
+                    >
+                </h1>
+            </div>
         </x-slot>
+        
 
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
